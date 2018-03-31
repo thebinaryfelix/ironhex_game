481cfa7c5ff5ab6c656d4a21ef9a6c446460c7c0
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-31 20:54:57 +0200

    draw and move player on canvas

--- js/player.js
+++ js/player.js
@@ -1,8 +1,8 @@
 var SPACE_KEY = 32;
 var LEFT_KEY = 37;
-var TOP_KEY = 38;
+var UP_KEY = 38;
 var RIGHT_KEY = 39;
-var BOTTOM_KEY = 40;
+var DOWN_KEY = 40;
 var W_KEY = 87;
 var S_KEY = 83;
 var A_KEY = 65;
@@ -17,17 +17,24 @@ function Player(game) {
 
   this._cellWidth = 50; //Changes with amount of Iron Snacks eaten
   this._cellHeight = 50; //Changes with amount of Iron Snacks eaten
+  
+  this._diagonal = 30;
+  this._side = Math.sqrt(
+    Math.pow(this._diagonal, 2) - Math.pow(this._diagonal / 2, 2)
+  );
 
-  this._position_X = 0; //Random on final game
-  this._position_Y = 0; //Random on final game
+  this._position_X = this._side;
+  this._position_Y = this._diagonal;
+
+  this._velocity_X = 10;
+  this._velocity_Y = 10;
 
   this._skill = new Skills(this); //Stores one or more skills gained by eating Iron Snacks
 
   this.setListener();
 }
 
-Player.prototype.score = function() {
-};
+Player.prototype.score = function() {};
 
 Player.prototype.setActiveSkill = function() {
   var skillIndex = Math.floor(this._score / 50);
@@ -38,48 +45,79 @@ Player.prototype.setListener = function() {
   document.onkeydown = function(pressedKey) {
     switch (pressedKey.keyCode) {
       case LEFT_KEY:
-        this.moveLeft(1);
+        this.moveLeft();
         break;
-      case TOP_KEY:
-        this.moveUp(1);
+      case UP_KEY:
+        this.moveUp();
         break;
       case RIGHT_KEY:
-        this.moveRight(1);
+        this.moveRight();
         break;
-      case BOTTOM_KEY:
-        this.moveDown(1);
-        break;
-      case A_KEY:
-        this.moveLeft(2);
-        break;
-      case W_KEY:
-        this.moveUp(2);
-        break;
-      case D_KEY:
-        this.moveRight(2);
-        break;
-      case S_KEY:
-        this.moveDown(2);
+      case DOWN_KEY:
+        this.moveDown();
         break;
     }
   }.bind(this);
 };
 
-Player.prototype.moveLeft = function(index) {
-  console.log("Moving left player " + index);
+Player.prototype.moveLeft = function() {
+  if ((this._position_X - this._side) <= 0) {
+    console.log(this._position_X);
+    return;
+  } else {
+    this._position_X -= this._velocity_X;
+  }
 };
 
-Player.prototype.moveUp = function(index) {
-  console.log("Moving up player " + index);
+Player.prototype.moveUp = function() {
+  if ((this._position_Y - this._diagonal) <= 0) {
+    return;
+  } else {
+    this._position_Y -= this._velocity_Y;
+  }
 };
 
-Player.prototype.moveRight = function(index) {
-  console.log("Moving right player " + index);
+Player.prototype.moveRight = function() {
+  if ((this._position_X + this._side) >= this._game._board.width) {
+    return;
+  } else {
+    this._position_X += this._velocity_X;
+  }
 };
 
-Player.prototype.moveDown = function(index) {
-  console.log("Moving down player " + index);
+Player.prototype.moveDown = function() {
+  if ((this._position_Y + this._diagonal) >= this._game._board.height) {
+    return;
+  } else {
+    this._position_Y += this._velocity_Y;
+  }
 };
 
-Player.prototype.savePlayerData = function(){
-}
+Player.prototype.savePlayerData = function() {};
+
+Player.prototype.draw = function() {
+  this._game._ctx.beginPath();
+  this._game._ctx.moveTo(this._position_X, this._position_Y - this._diagonal);
+  this._game._ctx.lineTo(
+    this._position_X + this._side,
+    this._position_Y - this._diagonal / 2
+  );
+  this._game._ctx.lineTo(
+    this._position_X + this._side,
+    this._position_Y + this._diagonal / 2
+  );
+  this._game._ctx.lineTo(this._position_X, this._position_Y + this._diagonal);
+  this._game._ctx.lineTo(
+    this._position_X - this._side,
+    this._position_Y + this._diagonal / 2
+  );
+  this._game._ctx.lineTo(
+    this._position_X - this._side,
+    this._position_Y - this._diagonal / 2
+  );
+  this._game._ctx.moveTo(this._position_X, this._position_Y - this._diagonal);
+  this._game._ctx.fill();
+  this._game._ctx.closePath();
+};
+
+Player.prototype.rotatingAttack = function() {};
