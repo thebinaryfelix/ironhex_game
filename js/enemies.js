c9da8048a210b85511c14d34dfd25052a8c70229
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-31 20:55:36 +0200

    random initial position and automatic move on canvas

--- js/enemies.js
+++ js/enemies.js
@@ -7,12 +7,24 @@ function Enemy(game) {
   this._cellWidth = 50; //Changes with amount of Iron Snacks eaten
   this._cellHeight = 50; //Changes with amount of Iron Snacks eaten
 
-  this._position_X = 0; //Random on final game
-  this._position_Y = 0; //Random on final game
+  this._position_X = Math.floor(Math.random()*(this._game._board.width - this._cellWidth) - this._cellWidth);
+  this._position_Y = Math.floor(Math.random()*(this._game._board.height - this._cellHeight) - this._cellHeight);
+
+  this._velocity_X = 1;
+  this._velocity_Y = 2;
 }
 
 Enemy.prototype.receiveDamage = function() {};
 
 Enemy.prototype.strength = function() {};
 
-Enemy.prototype.move = function() {};
+Enemy.prototype.move = function() {
+  this._position_X += this._velocity_X;
+  if(this._position_X <= 0 || this._position_X >= this._game._board.width){
+    this._velocity_X *= -1;
+  }
+  this._position_Y += this._velocity_Y;
+  if(this._position_Y <= 0 || this._position_Y >= this._game._board.height){
+    this._velocity_Y *= -1;
+  }
+};
