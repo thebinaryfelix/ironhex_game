c4c082a45402552a69600bddab31b8e5cf5055b8
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-01 03:21:24 +0200

    Deploy food randomly on game board

--- js/food.js
+++ js/food.js
@@ -1,14 +1,38 @@
-function Iron(boardName, ctx) {
-    this._board = boardName;
-    this._ctx = ctx;
+function Ironsnack(game) {
+    this._game = game;
 
-    this._energy = Math.floor(Math.random() * 50 + 10); //Random number between 10 and 50
+    this._energy = Math.floor(Math.random() * 15 + 5); //Random number between 10 and 50
   
-    //Bigger food gives more energy
-    this._cellWidth = this._energy * 2;
-    this._cellHeight = this._energy * 2;
+    this._diagonal = this._energy;
+    this._side = Math.sqrt(
+      Math.pow(this._diagonal, 2) - Math.pow(this._diagonal / 2, 2)
+    );
   
-    this._position_X = 0;
-    this._position_Y = 0;
+    this._position_X = Math.floor((Math.random() * (this._game._board.width - 2*this._side)) + this._side);
+    this._position_Y = Math.floor((Math.random() * (this._game._board.height - 2*this._diagonal)) + this._side);
   }
   
+  Ironsnack.prototype.draw = function() {
+    this._game._ctx.beginPath();
+    this._game._ctx.moveTo(this._position_X, this._position_Y - this._diagonal);
+    this._game._ctx.lineTo(
+      this._position_X + this._side,
+      this._position_Y - this._diagonal / 2
+    );
+    this._game._ctx.lineTo(
+      this._position_X + this._side,
+      this._position_Y + this._diagonal / 2
+    );
+    this._game._ctx.lineTo(this._position_X, this._position_Y + this._diagonal);
+    this._game._ctx.lineTo(
+      this._position_X - this._side,
+      this._position_Y + this._diagonal / 2
+    );
+    this._game._ctx.lineTo(
+      this._position_X - this._side,
+      this._position_Y - this._diagonal / 2
+    );
+    this._game._ctx.moveTo(this._position_X, this._position_Y - this._diagonal);
+    this._game._ctx.fill();
+    this._game._ctx.closePath();
+  };
