c70f95ff58039bf39e2f5c480184f74c5bb4a3c3
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-01 03:20:14 +0200

    Enemies on board

--- js/enemies.js
+++ js/enemies.js
@@ -1,14 +1,13 @@
 function Enemy(game) {
   this._game = game;
 
-  this._life = 100; //Increases with amount of food eaten
-  this._strength = 200; //More usefull against other players and protects from other player's attacks
+  this._life = 70; //Increases with amount of food eaten
+  this._strength = this._life*2; //More usefull against other players and protects from other player's attacks
 
-  this._cellWidth = 50; //Changes with amount of Iron Snacks eaten
-  this._cellHeight = 50; //Changes with amount of Iron Snacks eaten
+  this._radius = Math.floor(this._life/4);
 
-  this._position_X = Math.floor(Math.random()*(this._game._board.width - this._cellWidth) - this._cellWidth);
-  this._position_Y = Math.floor(Math.random()*(this._game._board.height - this._cellHeight) - this._cellHeight);
+  this._position_X = Math.floor(Math.random()*(this._game._board.width - 2*this._radius) + this._radius);
+  this._position_Y = Math.floor(Math.random()*(this._game._board.height - 2*this._radius) + this._radius);
 
   this._velocity_X = 1;
   this._velocity_Y = 1;
@@ -20,18 +19,19 @@ Enemy.prototype.strength = function() {};
 
 Enemy.prototype.move = function() {
   this._position_X += this._velocity_X;
-  if(this._position_X <= 0 || this._position_X >= this._game._board.width){
-    this._velocity_X *= -1;
-  }
   this._position_Y += this._velocity_Y;
-  if(this._position_Y <= 0 || this._position_Y >= this._game._board.height){
-    this._velocity_Y *= -1;
-  }
+    
+    if(this._position_X - this._radius <= 0 || this._position_X + this._radius >= this._game._board.width){
+      this._velocity_X *= -1;
+    }
+    if(this._position_Y - this._radius <= 0 || this._position_Y + this._radius >= this._game._board.height){
+      this._velocity_Y *= -1;
+    }
 };
 
 Enemy.prototype.draw = function(){
   this._game._ctx.beginPath();
-  this._game._ctx.arc(this._position_X, this._position_Y, 20, 0, 2 * Math.PI);
+  this._game._ctx.arc(this._position_X, this._position_Y, this._radius-2, 0, 2 * Math.PI);
   this._game._ctx.fill();
   this._game._ctx.closePath();
 }
